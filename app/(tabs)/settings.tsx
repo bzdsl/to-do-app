9f80ac94c730f91262f696c1fbe7db4a0bd092fa
Thang Duc Nguyen <75111229+bzdsl@users.noreply.github.com>
2025-08-06 14:48:39 +0700

    todos

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { Text, View } from "react-native";
+
+const settings = () => {
+  return (
+    <View>
+      <Text>settings</Text>
+    </View>
+  );
+};
+
+export default settings;
